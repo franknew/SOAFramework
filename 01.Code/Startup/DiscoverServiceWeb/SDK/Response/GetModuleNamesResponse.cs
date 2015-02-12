553e203f11e4c5a3cbf656f8c58f1a6d5a121241
franknew <liuxiao.a.boy@gmail.com>
2015-02-12 16:37:54 +0800

    aaa

---
+++ 01.Code/Startup/DiscoverServiceWeb/SDK/Response/GetModuleNamesResponse.cs
@@ -0,0 +1,13 @@
+﻿using SOAFramework.Service.SDK.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DiscoverServiceWeb.SDK.Response
+{
+    public class GetModuleNamesResponse : BaseResponse
+    {
+        public List<string> Items { get; set; }
+    }
+}
