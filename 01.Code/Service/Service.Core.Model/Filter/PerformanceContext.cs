fa9622ffe1439771b3e4ff3ee5557559fcccfa38
franknew <liuxiao.a.boy@gmail.com>
2015-02-12 16:31:59 +0800

    changes

---
+++ 01.Code/Service/Service.Core.Model/Filter/PerformanceContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOAFramework.Service.Core.Model
+{
+    /// <summary>
+    /// 性能监控上下文
+    /// </summary>
+    public class PerformanceContext
+    {
+        public long ElapsedMilliseconds { get; set; }
+    }
+}
