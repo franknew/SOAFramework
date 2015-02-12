284137a23fa475d20c3a0d0e67c16c009e6bbc87
franknew <liuxiao.a.boy@gmail.com>
2015-02-12 17:09:16 +0800

    aaa

---
+++ 01.Code/Service/Service.Core/Interface/IRequest.cs
@@ -0,0 +1,15 @@
+﻿using SOAFramework.Service.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOAFramework.Service.Interface
+{
+    [ServiceLayer(Enabled = false)]
+    public abstract class IRequest<T> where T : IResponse
+    {
+        public abstract string GetApiName();
+    }
+}
