3a0b1455b4d61f607da57b6600084cdd3a27f084
franknew <liuxiao.a.boy@gmail.com>
2015-06-07 21:05:57 +0800

    wcf singleton

--- 01.Code/Service/Service.Core/Server/SOAService.cs
+++ 01.Code/Service/Service.Core/Server/SOAService.cs
@@ -21,7 +21,7 @@ using SOAFramework.Service.Core.Model;
 namespace SOAFramework.Service.Server
 {
     // 注意: 使用“重构”菜单上的“重命名”命令，可以同时更改代码和配置文件中的类名“Service1”。
-    [ServiceBehavior(InstanceContextMode = InstanceContextMode.PerSession)]
+    [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
     public class SOAService : IService
     {
         private static IDispatcher _dispatcher = null;
